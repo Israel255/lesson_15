dd06f2a007094e2a880f7c26a8e592d6b0df3721
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-07-01 17:59:48 +0300

    Add files via upload

---
+++ LESSON_15_OOP/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSON_15_OOP
+{
+    class Game
+    {
+        public string _name;
+        public int _number_of_players;
+        public float _rating;
+        public string _origin_country;
+        public int _first_team_players;
+        public int _second_team_players;
+
+        public Game()
+        {
+
+        }
+
+        public Game(string name, float rating = 5.5f)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Game(string name, int number_of_players, float rating, string origin_country,int first_team_players,int second_team_players)
+        {
+            _name = name;
+            _number_of_players = number_of_players;
+            _rating = rating;
+            _origin_country = origin_country;
+            _first_team_players = first_team_players;
+            _second_team_players = second_team_players;
+        }
+
+        public Game(string name, string origin_country)
+        {
+            _name = name;
+            _origin_country = origin_country;
+        }
+
+        public int Tell_ME_How_Many_Players()
+        {
+            _number_of_players = _first_team_players + _second_team_players;
+            return _number_of_players;
+        }
+
+        public override string ToString()
+        {
+            return "Game Name: " + _name + " ,Number Of Players: " + _number_of_players + " ,Rating: " + _rating + " ,Origin Country: " + _origin_country;
+        }
+    }
+}
---
+++ LESSON_15_OOP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSON_15_OOP
+{
+    class Program
+    {
+        static void Print_Game(Game g)
+        {
+            int number = g.Tell_ME_How_Many_Players();
+            Console.WriteLine(g);
+        }
+        static void Main(string[] args)
+        {
+            Game soccer = new Game
+            {
+                 _name = "Soccer",
+                 _first_team_players = 11,
+                 _second_team_players = 11,
+                 //_number_of_players = 22,
+                 _origin_country = "England",
+                 _rating = 6.3f
+            };
+            soccer.Tell_ME_How_Many_Players();
+            Console.WriteLine(soccer);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Game basketball = new Game("Basketball")
+            {
+                _first_team_players = 5,
+                _second_team_players = 5,
+                //_number_of_players = 10,
+                _origin_country = "usa"
+            };
+            basketball.Tell_ME_How_Many_Players();
+            Console.WriteLine(basketball);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Game tennis = new Game("Tennis", "France")
+            {
+                 _first_team_players = 1,
+                 _second_team_players = 1,
+                 //_number_of_players = 2,
+                 _rating = 3.4f
+            };
+            tennis.Tell_ME_How_Many_Players();
+            Console.WriteLine(tennis);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Game wrestling = new Game("Wrestling", 2, 2.2f, "Greece",1,1);
+            Console.WriteLine(wrestling);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Print_Game(soccer);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Print_Game(basketball);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Print_Game(tennis);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Print_Game(wrestling);
+            Console.WriteLine("--------------------------------------------------------------------------");
+        }
+    }
+}
